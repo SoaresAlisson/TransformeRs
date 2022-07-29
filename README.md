24ca11c8fdbc6a134c04b82f2369d09fc07dd57f
SoaresAlisson <soares.alisson@yahooo.com.br>
2022-07-28 21:21:25 -0300

    1st commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # TransformeRs
-Some implementations of Transformers/Bert in R
+
+Some implementations of Transformers/Bert in R, mainly in Portuguese language.
+
+At the moment, we have only one implementation:
+
+## bert-restore-punctuation-ptbr
+
+The [bert-restore-punctuation-ptbr_in_R](scripts/bert-restore-punctuation-ptbr_in_R.R)
+is a script in R to use the transformer/BERT model from huggingface [bert-restore-punctuation-ptbr](https://huggingface.co/dominguesm/bert-restore-punctuation-ptbr) that restore punctuation and case. For example:
+
+> henrique foi no lago pescar com o pedro mais tarde foram para a casa do pedro fritar os peixes
+
+Using bert-restore-punctuation-ptbr is turned into:
+
+> Henrique foi no lago pescar com o Pedro. Mais tarde, foram para a casa do Pedro fritar os peixes.
+
+Because it is in huggingface, is also possible to test the model direct in site (look at the "Hosted inference API")
+
+Soon a vignette on how to use it will be posted.
---
+++ bert-restore-punctuation-ptbr_in_R.R
@@ -0,0 +1,41 @@
+# This is a script to use in R the transformer/BERT model from huggingface 
+# https://huggingface.co/dominguesm/bert-restore-punctuation-ptbr that restore 
+# punctuation and case.
+#
+# I'm very helpful to https://github.com/hhmacedo and https://huggingface.co/dominguesm
+# that helped me to make this script
+#
+# Soon a vignette will be available
+
+
+reconstructBERT <- function(texto){
+  
+  df <- do.call(rbind.data.frame, modeloBert(texto)) |> 
+    dplyr::select(entity, word) 
+  
+  while (nrow(filter(df, grepl("^##", word))) > 0) {
+    df <- df |>
+      dplyr::mutate(word2 =
+                      case_when(!grepl("^##", word) & !grepl("^##", lead(word)) ~ word,
+                                grepl("^##", word) & !grepl("^##", lag(word)) ~ 
+                                  paste0(lag(word), substr(word, 3, length(word))),
+                                grepl("^##", word) & grepl("^##", lag(word)) ~ word)) |>
+      dplyr::filter(!is.na(word2)) |>
+      dplyr::mutate(word = word2, word2 = NULL)
+  }
+  df |> 
+    rbind(c( NA, NA)) |>
+    # Converting cases and adding punctuation
+    dplyr::mutate(converted = word,
+                  converted = dplyr::if_else(
+                    stringr::str_detect(entity, ".U"),
+                    stringr::str_to_title(converted), converted),
+                  converted = dplyr::if_else(
+                    stringr::str_detect(entity, "\\W."),
+                    stringr::str_replace(converted, "$",
+                                         stringr::str_extract(entity, "\\W")),
+                    converted)) |> 
+    dplyr::filter(!lead(grepl("##", word)))  |># strip "broken" words
+    dplyr::summarise(frase = paste(converted, collapse = " ")) |>
+    dplyr::pull(frase)
+}
